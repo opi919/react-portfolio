3f57a6ec5a6e24d12e09f20890289fd28689b9a4
opi919 <khandokermomen919@gmail.com>
2022-10-28 19:41:32 +0600

    about page added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters/index'
 import Logo from '../Home/Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -13,30 +14,33 @@ const Home = () => {
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
-        }, 2000)
+        }, 4000)
     }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={`${letterClass} _10`}>H</span>
-                    <span className={`${letterClass} _11`}>i</span>
-                    <span className={`${letterClass} _12`}>,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'</span>
-                    <span className={`${letterClass} _15`}>m</span>
-                    <img src={Title} alt="title" />
-                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={16} />
-                    <br />
-                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20} />
-                </h1>
-                <h2>Frontend Developer / Backend Developer</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={`${letterClass} _10`}>H</span>
+                        <span className={`${letterClass} _11`}>i</span>
+                        <span className={`${letterClass} _12`}>,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'</span>
+                        <span className={`${letterClass} _15`}>m</span>
+                        <img src={Title} alt="title" />
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={16} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20} />
+                    </h1>
+                    <h2>Frontend Developer / Backend Developer</h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type='pacman' />
+        </>
     )
 }
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/mop.png'
 import LogoSub from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome,faUser,faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
@@ -13,13 +13,13 @@ const Sidebar = () => (
             {/* <img className='sub-logo' src={LogoSub} alt="subtitle" /> */}
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact activeclassname="active" to="/" >
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" className='about-link' activeclassname="active" to="/about">
+            <NavLink exact activeclassname="active" className='about-link' to="about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" className='contact-link' activeclassname="active" to="/contact">
+            <NavLink exact activeclassname="active" className='contact-link' to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
